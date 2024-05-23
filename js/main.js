37b32ef33e4aab7f91adfe35665ac8c2da5ad9bf
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-05-23 11:27:04 -0600

    added API
    Looking to collect Item data

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ function copy(){
 	document.querySelector('#rarity').innerText = rare
 	document.querySelector('#value').innerText = val
 }
+
+// API Collection
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/equipment/`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
