3174674be7f74c39bebca402c306bc74ae9c37ce
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-07-17 16:59:49 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,15 @@
-document.querySelector('#button').addEventListener('click', copy)
+ API Collection
+document.querySelector('button').addEventListener('click',copy)
 
-function copy(){
-	let item = document.querySelector('#item').value
-	let name = document.querySelector('#name').value
-	let metal = document.querySelector('#metal').value
-	let rare = document.querySelector('#rare').value
-	let val = document.querySelector('#val').value
-	document.querySelector('#description').innerText = item
-	document.querySelector('#title').innerText = name
-	document.querySelector('#type').innerText = metal
-	document.querySelector('#rarity').innerText = rare
-	document.querySelector('#value').innerText = val
-}
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/equipment/`
 
-// API Collection
-//document.querySelector('button').addEventListener('click',copy)
-
-//function getFetch(){
-//  const choice = document.querySelector('input').value
-//  const url = `https://www.dnd5eapi.co/api/equipment/`
-
-//  fetch(url)
-//      .then(res => res.json()) // parse response as JSON
-//      .then(data => {
-//        console.log(data)
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
       })
-//      .catch(err => {
-//          console.log(`error ${err}`)
-//      });
+      .catch(err => {
+          console.log(`error ${err}`)
+     });
